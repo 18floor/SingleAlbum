6d3f837bcb45f51783cd0bf072e55a759469e23f
Aleksey Volkov <cface@yandex.ru>
2021-05-16 22:41:56 +0300

    fix dto & adapter

--- app/src/main/java/ru/netology/singlealbum/adapter/AlbumAdapter.kt
+++ app/src/main/java/ru/netology/singlealbum/adapter/AlbumAdapter.kt
@@ -5,10 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import ru.netology.singlealbum.databinding.ItemAlbumBinding
 import ru.netology.singlealbum.databinding.ItemTrackBinding
-import ru.netology.singlealbum.dto.Album
-import ru.netology.singlealbum.dto.AlbumItem
 import ru.netology.singlealbum.dto.Track
 
 interface OnInteractionListener {
@@ -17,90 +14,41 @@ interface OnInteractionListener {
 
 class AlbumAdapter(
     private val onInteractionListener: OnInteractionListener
-) : ListAdapter<AlbumItem, RecyclerView.ViewHolder>(AlbumDiffCallback()) {
+) : ListAdapter<Track, TrackViewHolder>(TrackDiffCallback()) {
 
-    private val typeHeader = 0
-    private val typeTrack = 1
-
-    override fun getItemViewType(position: Int): Int {
-        return when (getItem(position)) {
-            is Album -> typeHeader
-            is Track -> typeTrack
-            null -> throw IllegalArgumentException("unknown item type")
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val binding = ItemTrackBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TrackViewHolder(binding, onInteractionListener)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        return when (viewType) {
-            typeHeader -> HeaderViewHolder(
-                ItemAlbumBinding.inflate(layoutInflater, parent, false)
-            )
-            typeTrack -> TrackViewHolder(
-                ItemTrackBinding.inflate(layoutInflater, parent, false),
-                onInteractionListener
-            )
-            else -> throw IllegalArgumentException("unknown view type: $viewType")
-        }
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        val track = getItem(position)
+        holder.bind(track)
     }
+}
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        getItem(position)?.let {
-            when (it) {
-                is Album -> (holder as? HeaderViewHolder)?.bind(it)
-                is Track -> (holder as? TrackViewHolder)?.bind(it)
+class TrackViewHolder(
+    private val binding: ItemTrackBinding,
+    private val onInteractionListener: OnInteractionListener
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(track: Track) {
+
+        binding.apply {
+            trackName.text = track.file
+            playPauseButton.setOnClickListener {
+                onInteractionListener.onPlayPause(track)
             }
-        }
-    }
-
-    override fun getItemCount(): Int {
-        return super.getItemCount()
-    }
-
-    class TrackViewHolder(
-        private val binding: ItemTrackBinding,
-        private val onInteractionListener: OnInteractionListener
-    ) : RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(track: Track) {
-
-            binding.apply {
-                trackName.text = track.file
-                playPauseButton.setOnClickListener {
-                    onInteractionListener.onPlayPause(track)
-                }
-            }
-        }
-    }
-
-    class HeaderViewHolder(
-        private val binding: ItemAlbumBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(album: Album) {
-
-            binding.apply {
-                titleView.text = album.title
-                artistView.text = album.artist
-                subtitleView.text = album.subtitle
-                publishedView.text = album.published
-                genreView.text = album.genre
-
-            }
-        }
-    }
-
-    class AlbumDiffCallback : DiffUtil.ItemCallback<AlbumItem>() {
-        override fun areItemsTheSame(oldItem: AlbumItem, newItem: AlbumItem): Boolean {
-            if (oldItem::class != newItem::class) {
-                return false
-            }
-            return oldItem.id == newItem.id
-        }
-
-        override fun areContentsTheSame(oldItem: AlbumItem, newItem: AlbumItem): Boolean {
-            return oldItem == newItem
         }
     }
 }
 
+class TrackDiffCallback : DiffUtil.ItemCallback<Track>() {
+    override fun areItemsTheSame(oldItem: Track, newItem: Track): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Track, newItem: Track): Boolean {
+        return oldItem == newItem
+    }
+}
--- app/src/main/java/ru/netology/singlealbum/dto/Entity.kt
+++ app/src/main/java/ru/netology/singlealbum/dto/Entity.kt
@@ -1,20 +1,16 @@
 package ru.netology.singlealbum.dto
 
-sealed class AlbumItem {
-    abstract val id: Int
-}
-
 data class Album(
-    override val id: Int,
+    val id: Int,
     val title: String,
     val subtitle: String,
     val artist: String,
     val published: String,
     val genre: String,
     val tracks: List<Track>
-) : AlbumItem()
+)
 
 data class Track(
-    override val id: Int,
+    val id: Int,
     val file: String,
-) : AlbumItem()
+)
