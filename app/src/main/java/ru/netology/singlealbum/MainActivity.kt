24a6a2a0c61396f91cd8161c45928d67a2d45d69
Aleksey Volkov <cface@yandex.ru>
2021-05-18 00:20:19 +0300

    add MediaLifecycleObserver

--- app/src/main/java/ru/netology/singlealbum/MainActivity.kt
+++ app/src/main/java/ru/netology/singlealbum/MainActivity.kt
@@ -1,10 +1,13 @@
 package ru.netology.singlealbum
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DividerItemDecoration
+import ru.netology.singlealbum.BuildConfig.BASE_URL
 import ru.netology.singlealbum.adapter.AlbumAdapter
 import ru.netology.singlealbum.adapter.OnInteractionListener
 import ru.netology.singlealbum.databinding.ActivityMainBinding
@@ -20,10 +23,51 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val mediaPlayer = MediaPlayer().apply {
+            setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .build()
+            )
+//                    setDataSource(url)
+//                    prepare()
+//                    start()
+        }
+
 
         val adapter = AlbumAdapter(object : OnInteractionListener {
             override fun onPlayPause(track: Track) {
-                super.onPlayPause(track)
+
+                val url = BASE_URL + track.file
+
+
+                if (mediaPlayer.isPlaying) {
+                    mediaPlayer.release()
+                } else {
+                    mediaPlayer.setDataSource(url)
+                    mediaPlayer.prepare()
+                    mediaPlayer.start()
+                }
+
+
+
+
+
+//                mediaObserver.apply {
+//                    player?.setDataSource(
+//                        BASE_URL + track.file
+//                    )
+//                }.let {
+//
+//                    if (it.player?.isPlaying == true) {
+//                        it.player?.pause()
+//                    } else {
+//                        it.play()
+//                    }
+//
+//                }
+
             }
         })
 
